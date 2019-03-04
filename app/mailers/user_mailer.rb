729cf943383cb98c442c575f3948c1121852393d
jiaad <abdulziad08@outlook.com>
2019-03-04 16:35:44 +0100

    mailer works, user recive mail when he signed up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,9 @@ class UserMailer < ApplicationMailer
   # default from: 'notifications@example.com'
 
   def welcome_email(user)
+  
     @user = user
     @url ='https://github.com/jiaad'
-    mail(to: @user.email, subject:'Welcome to My Awesome')
+    mail(to: @user.email, subject:'Welcome to My Site', delivary_method_options:{version: 'v3.1', api_key: ENV['MAIL_JET_API_PUBLIQUE'], secret_key:ENV['MAIL_JET_API_SECRET']})
   end
 end
