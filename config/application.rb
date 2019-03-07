7f9e666aaa8ccf1f6daeb2d856a0eef78e8d69b6
TheMindset <mulezig2014@outlook.fr>
2019-03-07 15:38:15 +0100

    change 4

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ module ProjectEcommerce
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_mailer.delivery_method = :mailjet
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
