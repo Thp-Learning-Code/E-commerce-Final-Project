b29e5e13481c65698bda9f0cc8be6dc9675298be
slim2244 <seb_93@live.fr>
2019-03-07 22:08:21 +0100

    ajout mailer text

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,20 @@ Picture.delete_all
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
 ActiveRecord::Base.connection.reset_pk_sequence!('pictures')
 
-5.times do |i|
+2.times do |i|
   User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: 'qwerty', email: "user#{i}@yopmail.com", password: 'qwerty')
 end
 
 puts 'Users created'
-User.create!(first_name:"gros",last_name:"gros", description:"je suis admin", email:"jiad@jiad.com", password:123456)
 
-# 5.times do |_i|
-#   Picture.create!(name: Faker::Name.first_name, description: 'Moi,Moche et Méchant. Allez acheter cette putain de photo !', price: rand(1..30), condition: 'good', administrator_id: User.all.sample.id)
-# end
-# puts 'Pictures created'
+2.times do |i|
+  User.create!(first_name:"gros",last_name:"gros", description:"je suis admin", email:"admin#{i}@admin.com", password:"qwerty", is_superadmin: true)
+end
+
+puts 'Users created'
+
+2.times do |_i|
+  Picture.create!(name: Faker::Name.first_name, description: 'Moi,Moche et Méchant. Allez acheter cette putain de photo !', price: rand(1..30), condition: 'good', administrator_id: User.all.sample.id)
+end
+
+puts 'Pictures created'
